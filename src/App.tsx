6eff59ac2def623f6dfbc659e6fea8d0339a74f7
Pawel Urasinski <uraspawel@gmail.com>
2023-09-12 21:22:50 +0200

    Made reusable custom components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 // App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom';
-import { CssBaseline } from '@mui/material';
 import { Home } from './screens/Home';
-import Login from './screens/Login';
-import HotelList from './screens/HotelList';
+import { Login } from './screens/Login';
+import { HotelsList } from './screens/HotelsList';
 import HotelDetails from './screens/HotelDetails';
 import HotelRoomList from './screens/HotelRoomList';
 import HotelRoomDetails from './screens/HotelRoomDetails';
+import { CssBaseline} from '@mui/material';
 
 export const App = () => {
 	return (
@@ -16,7 +16,7 @@ export const App = () => {
 			<Switch>
 				{/* <Route path='/' element={<Home />} /> */}
 				<Route path='/login' element={<Login />} />
-				<Route path='/hotels' element={<HotelList />} />
+				<Route path='/hotels' element={<HotelsList />} />
 				<Route path='/hotel/:hotelId' element={<HotelDetails />} />
 				<Route path='/hotel/:hotelId/rooms' element={<HotelRoomList />} />
 				<Route path='/hotel/:hotelId/room/:roomId' element={<HotelRoomDetails />} />
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
-import { Container, TextField, Button, Avatar, CssBaseline, Box } from '@mui/material';
+import {
+	Container,
+	TextField,
+	Button,
+	Avatar,
+	Box,
+	ThemeProvider,
+	CssBaseline,
+	useTheme,
+	createTheme,
+	styled,
+	makeStyles,
+} from '@mui/material';
 
 import { useNavigate } from 'react-router-dom';
+import { GlobalTheme } from '../themes/GlobalTheme';
 
-const Login = () => {
+export const Login = () => {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 	const navigate = useNavigate();
@@ -18,126 +31,120 @@ const Login = () => {
 		}
 	};
 
+	const theme = createTheme({
+		palette: {
+			primary: {
+				main: '#EEF4F5', // Ustaw kolor główny
+			},
+			background: {
+				default: '#3F7A29', // Ustaw kolor tła
+			},
+			text: {
+				primary: '#3F7A29', // Ustaw kolor tekstu głównego
+			},
+		},
+	});
+
+	const CustomContainer = styled(Container)({
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+	});
+
+	const CustomInput = styled(TextField)({
+		'& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+			borderColor: theme.palette.background.default,
+		},
+
+		'& .MuiOutlinedInput-root': {
+			'&.Mui-focused fieldset': {
+				borderColor: theme.palette.background.default,
+			},
+		},
+
+		'& .MuiOutlinedInput-input': {
+			color: theme.palette.background.default,
+		},
+		'& .MuiInputBase-input': {
+			height: 28,
+		},
+		'& label.Mui-focused': {
+			color: theme.palette.background.default,
+		},
+		'& .MuiInputLabel-outlined': {
+			color: theme.palette.background.default,
+		},
+	});
+
 	return (
-		<Container
-			component='main'
-			sx={{
-				backgroundColor: '#EEF4F5',
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-				justifyContent: 'center',
-				padding: '0px',
-				height: '100vh',
-			}}
-		>
-			<Box
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<CustomContainer
+				// component='main'
 				sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					justifyContent: 'center',
-					height: '800px',
+					backgroundColor: theme.palette.primary.main,
+					padding: '0px',
+					height: '100vh',
 				}}
 			>
-				<Avatar
-					alt='Hotel Service Logo'
-					src='./images/Hotelservice+GmbH+ 1.png'
-					variant='square'
+				<CustomContainer
 					sx={{
-						width: '150px',
-						height: '150px',
-						mb: '4rem',
-						mt: '-7.5rem',
-					}}
-				></Avatar>
-
-				<TextField
-					label='E-mail'
-					type='email'
-					variant='outlined'
-					size='small'
-					value={username}
-					onChange={e => setUsername(e.target.value)}
-					sx={{
-						'& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-							borderColor: '#3F7A29',
-						},
-
-						'& .MuiOutlinedInput-root': {
-							'&.Mui-focused fieldset': {
-								borderColor: '#3F7A29',
-							},
-						},
-
-						'& .MuiOutlinedInput-input': {
-							color: '#3F7A29',
-						},
-						'& .MuiInputBase-input': {
-							height: 28,
-						},
-						'& label.Mui-focused': {
-							color: '#3F7A29',
-						},
-						'& .MuiInputLabel-outlined': {
-							color: '#3F7A29',
-						},
-						width: 290,
-						mb: '25px',
-					}}
-				/>
-				<TextField
-					label='Hasło'
-					type='password'
-					variant='outlined'
-					size='small'
-					value={password}
-					onChange={e => setPassword(e.target.value)}
-					sx={{
-						'& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-							borderColor: '#3F7A29',
-						},
-
-						'& .MuiOutlinedInput-root': {
-							'&.Mui-focused fieldset': {
-								borderColor: '#3F7A29',
-							},
-						},
-
-						'& .MuiOutlinedInput-input': {
-							color: '#3F7A29',
-						},
-						'& .MuiInputBase-input': {
-							height: 28,
-						},
-						'& label.Mui-focused': {
-							color: '#3F7A29',
-						},
-						'& .MuiInputLabel-outlined': {
-							color: '#3F7A29',
-						},
-						width: 290,
-					}}
-				/>
-				<Button
-					onClick={handleLogin}
-					variant='contained'
-					color='primary'
-					sx={{
-						backgroundColor: '#3F7A29',
-						marginTop: '70px',
-						py: '10.25px',
-						width: 290,
-						'& .MuiButton-root': {
-							height: 28,
-						},
+						height: '800px',
 					}}
 				>
-					Zaloguj
-				</Button>
-			</Box>
-		</Container>
+					<Avatar
+						alt='Hotel Service Logo'
+						src='./images/Hotelservice+GmbH+ 1.png'
+						variant='square'
+						sx={{
+							width: '150px',
+							height: '150px',
+							mb: '4rem',
+							mt: '-7.5rem',
+						}}
+					></Avatar>
+
+					<CustomInput
+						label='E-mail'
+						type='email'
+						variant='outlined'
+						size='small'
+						value={username}
+						onChange={e => setUsername(e.target.value)}
+						sx={{
+							width: 290,
+							mb: '25px',
+						}}
+					/>
+					<CustomInput
+						label='Hasło'
+						type='password'
+						variant='outlined'
+						size='small'
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+						sx={{
+							width: 290,
+						}}
+					/>
+					<Button
+						onClick={handleLogin}
+						variant='contained'
+						sx={{
+							backgroundColor: theme.palette.background.default,
+							marginTop: '70px',
+							py: '10.25px',
+							width: 290,
+							'& .MuiButton-root': {
+								height: 28,
+							},
+						}}
+					>
+						Zaloguj
+					</Button>
+				</CustomContainer>
+			</CustomContainer>
+		</ThemeProvider>
 	);
 };
-
-export default Login;
