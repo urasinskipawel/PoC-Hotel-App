fd7a347977b6c356edc9b5ed86a946cafb875143
Pawel Urasinski <uraspawel@gmail.com>
2023-12-05 18:10:27 +0100

    Changed in-line styles in CustomButton component

--- src/components/CustomButton/CustomButtom.tsx
+++ src/components/CustomButton/CustomButtom.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
 interface CustomButtonProps {
 	disabled?: boolean;
@@ -8,26 +9,28 @@ interface CustomButtonProps {
 	btnName: string;
 }
 
+const useStyles = makeStyles(theme => ({
+	root: {
+		backgroundColor: (props: CustomButtonProps) => props.btnBackground,
+		margin: '75px 0px 50px 0px',
+		color: '#EEF4F5',
+		paddingY: '10.25px',
+		minWidth: '290px',
+		'& .MuiButton-root': {
+			height: 28,
+		},
+		'&.Mui-disabled': {
+			backgroundColor: (props: CustomButtonProps) => props.disabledBackground,
+			color: '#EEF4F5',
+		},
+	},
+}));
+
 export const CustomButton = ({ disabled, btnBackground, disabledBackground, btnName }: CustomButtonProps) => {
+	const classes = useStyles({ btnBackground, disabledBackground, btnName });
+
 	return (
-		<Button
-			type='submit'
-			disabled={disabled}
-			variant='contained'
-			sx={{
-				backgroundColor: btnBackground,
-				margin: '75px 0px 50px 0px',
-				color: '#EEF4F5',
-				py: '10.25px',
-				minWidth: '290px',
-				'& .MuiButton-root': {
-					height: 28,
-				},
-				'&.Mui-disabled': {
-					background: disabledBackground,
-					color: '#EEF4F5',
-				},
-			}}>
+		<Button type='submit' disabled={disabled} variant='contained' className={classes.root}>
 			{btnName}
 		</Button>
 	);
